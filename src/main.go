f1a0156b2e2447581d15c125299816a5d75c1ed6
hiromu.saito <hiromu.saito@bigtreetc.com>
2020-11-15 22:33:28 +0900

    メッセージを送ったときに定型文を返送するよう実装

--- src/main.go
+++ src/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/linebot/src/config"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/callback", callback)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello world--------------")
+const messageBack = "位置情報を送信してください。"
+
+func callback(w http.ResponseWriter, r *http.Request) {
+	bot, err := linebot.New(config.SECRET, config.TOKEN)
+	if err != nil {
+		log.Fatal(err)
+	}
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400) //Bad Request
+		} else {
+			w.WriteHeader(500) //Internal Server Error
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch event.Message.(type) {
+			//テキストを受信した場合
+			case *linebot.TextMessage:
+				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(messageBack)).Do()
+				if err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
